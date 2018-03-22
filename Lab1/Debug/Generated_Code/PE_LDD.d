eb24b72d81621add826c9e5162dd5b525b914483
PMcL <peter.mclean@uts.edu.au>
2018-03-23 01:36:52 +1100

    The Lab 1 template
    Signed-off-by: PMcL <peter.mclean@uts.edu.au>

---
+++ Lab1/Debug/Generated_Code/PE_LDD.d
@@ -0,0 +1,19 @@
+Generated_Code/PE_LDD.o: ../Generated_Code/PE_LDD.c \
+ ../Generated_Code/PE_LDD.h ../Generated_Code/PE_Types.h \
+ ../Generated_Code/PE_Error.h ../Generated_Code/PE_Const.h \
+ ../Generated_Code/Cpu.h ../Generated_Code/IO_Map.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Static_Code/IO_Map/MK70F12.h
+
+../Generated_Code/PE_LDD.h:
+
+../Generated_Code/PE_Types.h:
+
+../Generated_Code/PE_Error.h:
+
+../Generated_Code/PE_Const.h:
+
+../Generated_Code/Cpu.h:
+
+../Generated_Code/IO_Map.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Static_Code/IO_Map/MK70F12.h:
---
+++ Lab1/Debug/Sources/main.d
@@ -0,0 +1,19 @@
+Sources/main.o: ../Sources/main.c \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/Cpu.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Types.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Error.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Const.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/IO_Map.h \
+ C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Static_Code/IO_Map/MK70F12.h
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/Cpu.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Types.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Error.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/PE_Const.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Generated_Code/IO_Map.h:
+
+C:/Users/PMcL/Documents/Subjects/48434\ Embedded\ Software/4\ Labs/Lab\ 1/Template/Lab1/Static_Code/IO_Map/MK70F12.h:
