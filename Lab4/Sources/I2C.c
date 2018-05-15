74feb2b735658c0eb8eafad10724dbfcd65074c9
12551519 <12551519@student.uts.edu.au>
2018-05-15 19:44:35 +1000

    fixed I2C comments and tested exhaustive search successfully

--- Lab4/Sources/I2C.c
+++ Lab4/Sources/I2C.c
@@ -133,20 +133,19 @@ bool I2C_Init(const TI2CModule* const aI2CModule, const uint32_t moduleClk)
   // Enable open drain
   PORTE_PCR19 |= PORT_PCR_ODE_MASK;
 
-  /* Set I2C Frequency Divider Register  TODO: Write a private function that performs an exhaustive search for these parameters
+  /* Set I2C Frequency Divider Register
    * MULT: 0x02;   mul = 4;
    * ICR:  0x12;  SCL divider = 64;  SDA hold value = 13;  SCL hold (start) = 26;  SCL hold (stop) = 33;
-   * I2C baud rate = 25Mhz / (4 * 640 = 97,656 bits/second
-   */ //TODO: Test properly, neaten up, remove comments above, also maybe check if result if acceptable otherwise return false??
+   * I2C baud rate = 25Mhz / (4 * 640) = 97,656 bits/second
+   */
 
   // Array to store mul and icr
-
   uint8_t mulArray[]  = {1,2,4};
-  uint16_t sclArray[] = {20,22,24,26,28,30,34,40,28,32,36,40,44,48,56,68,48,
-                         56,64,72,80,88,104,128,80,96,112,128,144,160,192,
-			 240,160,192,224,256,288,320,384,448,512,576,640,768,
-			 960,640,768,896,1024,1152,1280,1536,1920,1280,1536,
-			 1792,2048,2304,2560,3072,3840};
+  uint16_t sclArray[] = {20,22,24,26,28,30,34,40,28,32,36,40,44,48,56,68,48,56,
+                         64,72,80,88,104,128,80,96,112,128,144,160,192,240,160,
+                         192,224,256,288,320,384,448,512,576,640,768,960,640,
+                         768,896,1024,1152,1280,1536,1920,1280,1536,1792,2048,
+                         2304,2560,3072,3840};
 
   // Variables to store best matches, the difference and the test difference
   uint8_t bestMul = 0;
@@ -160,7 +159,7 @@ bool I2C_Init(const TI2CModule* const aI2CModule, const uint32_t moduleClk)
     {
       testBaudRate = moduleClk / (mulArray[i] * sclArray[j]);
 
-      // If the testBaudRate is smaller than or equal to the desired baud rate
+      // Check if absolute value of testBaudRate is closer to desired baud rate
       if(aI2CModule->baudRate >= testBaudRate)
       {
         if((aI2CModule->baudRate - testBaudRate) < baudRateDifference)
@@ -170,7 +169,6 @@ bool I2C_Init(const TI2CModule* const aI2CModule, const uint32_t moduleClk)
           bestScl = j;
         }
       }
-      // IF the testBaudRate is larger than the desired baud rate
       else if (aI2CModule->baudRate < testBaudRate)
       {
         if((testBaudRate - aI2CModule->baudRate) < baudRateDifference)
@@ -182,7 +180,8 @@ bool I2C_Init(const TI2CModule* const aI2CModule, const uint32_t moduleClk)
       }
     }
   }
-
+  
+  // Set baud rate
   I2C0_F = I2C_F_MULT(bestMul);
   I2C0_F = I2C_F_ICR(bestScl);
 
